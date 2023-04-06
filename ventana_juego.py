4d3b064ff9ac5691bb178992ca6b3812139edff7
Jeison Johel Picado Picado <jeijoel@estudiantec.cr>
2023-04-06 13:29:05 -0600

    La nave no sale de la pantalla al moverse

--- ventana_juego.py
+++ ventana_juego.py
@@ -11,7 +11,7 @@
         ******************************************************************************************************
 """
 from tkinter import * #librería de la interfaz gráfica a utilizar
-import time
+
 
 #Crea la ventana del juego
 ventana_juego = Tk()
@@ -30,25 +30,41 @@ def mover_nave_arriba(event):
     """
     Mueve la nave de su posición actual a una posición mas arriba dentro del lienzo del juego
     """
-    lienzo_juego.move("nave_jugador", 0, -10)
+    y1 = lienzo_juego.bbox('nave_jugador')[1]#.bbox retorna una tupla (x1, y1, x2, y2) donde los primeros dos valores son la esquina superior izquierda del rectángulo y los ultimos dos la esquina inferior derecha del rectángulo
+    if y1 > 11:
+        lienzo_juego.move("nave_jugador", 0, -10)
+    elif y1 > 1:
+        lienzo_juego.move("nave_jugador", 0, -1)
 
 def mover_nave_abajo(event):
     """
     Mueve la nave de su posición actual a una posición mas abajo dentro del lienzo del juego
     """
-    lienzo_juego.move("nave_jugador", 0, 10)
+    y2 = lienzo_juego.bbox('nave_jugador')[3]#.bbox retorna una tupla (x1, y1, x2, y2) donde los primeros dos valores son la esquina superior izquierda del rectángulo y los ultimos dos la esquina inferior derecha del rectángulo
+    if y2 < 568:
+        lienzo_juego.move("nave_jugador", 0, 10)
+    elif y2 < 578:
+        lienzo_juego.move("nave_jugador", 0, 1)
 
 def mover_nave_izquierda(event):
     """
     Mueve la nave de su posición actual a una posición mas a la izquierda dentro del lienzo del juego
     """
-    lienzo_juego.move("nave_jugador", -10, 0)
+    x1 = lienzo_juego.bbox('nave_jugador')[0]#.bbox retorna una tupla (x1, y1, x2, y2) donde los primeros dos valores son la esquina superior izquierda del rectángulo y los ultimos dos la esquina inferior derecha del rectángulo
+    if x1 > 11:
+        lienzo_juego.move("nave_jugador", -10, 0)
+    elif x1 > 1:
+        lienzo_juego.move("nave_jugador", -1, 0)
 
 def mover_nave_derecha(event):
     """
     Mueve la nave de su posición actual a una posición mas a la derecha del lienzo del juego
     """
-    lienzo_juego.move("nave_jugador", 10, 0)
+    x2 = lienzo_juego.bbox('nave_jugador')[2]#.bbox retorna una tupla (x1, y1, x2, y2) donde los primeros dos valores son la esquina superior izquierda del rectángulo y los ultimos dos la esquina inferior derecha del rectángulo
+    if x2 < 768:
+        lienzo_juego.move("nave_jugador", 10, 0)
+    elif x2 < 778:
+        lienzo_juego.move("nave_jugador", 1, 0)
 
 #Eventos del movimiento de la nave
 lienzo_juego.bind('<Up>', mover_nave_arriba) #Al presionar la flecha hacia arriba estando en el lienzo juego mueve la nave hacia arriba
@@ -57,5 +73,6 @@ lienzo_juego.bind('<Left>', mover_nave_izquierda) #Al presionar la flecha hacia 
 lienzo_juego.bind('<Right>', mover_nave_derecha) #Al presionar la flecha hacia arriba estando en el lienzo juego mueve la nave hacia la derecha
 
 lienzo_juego.focus_set()
+
 
 ventana_juego.mainloop()
